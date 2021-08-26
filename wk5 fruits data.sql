b709d83eb382eca3bb32f2a1e191df4b29f550c0
JadeChung <85326117+JadeChung@users.noreply.github.com>
2021-08-25 19:47:13 -0700

    Add files via upload

---
+++ wk5 fruits data.sql
@@ -0,0 +1,7 @@
+USE fruits_db;
+INSERT INTO fruit (scienctific_name, common_name)
+VALUES
+('Malus domestica', 'Apple'),
+('Prunus persica', 'Peach'),
+('Citrus reticulata', 'Mandarin orange'),
+('Ananas comosus', 'Pineapple');
